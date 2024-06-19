dbdd95039100c4e9cc7abcef737762c3e4aea1c1
Kalle Pohjanjoki <kalle@pohjanjoki.com>
2024-06-19 10:44:28 +0300

    Initial Commit

---
+++ MultiplicationTableGame/MultiplicationTableGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTableGameApp.swift
+//  MultiplicationTableGame
+//
+//  Created by Kalle Pohjanjoki on 19.6.2024.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTableGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
